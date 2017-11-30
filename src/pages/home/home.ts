ff9e52838f13539d414d00ab159a979d15b97648
kevin frostad <ratorme@gmail.com>
2017-11-30 14:40:38 +0100

    k

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,54 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import * as mqtt from 'mqtt';
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
 
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  status = false;
+  isRecording = true;
+  motion = true;
 
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    let options = {
+      port : 33521,
+      host : 'm23.cloudmqtt.com',
+      username : 'cxuimdlz',
+      password : 'qYs1sz9Y31a9'
+    }
+
+    let client = mqtt.connect( 'mqtts://m23.cloudmqtt.com', options);
+
+    client.on('close', func => {
+      this.status = false;
+    })
+
+    client.on('connect', func => {
+      client.subscribe('device/pir');
+      this.status = true;
+    });
+
+    client.on('message', (topic, msg) => {
+      if (topic == "device/pir") {
+        if (msg.toString() == "1")
+          this.motion = true;
+      }
+    });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
   }
 
 }
